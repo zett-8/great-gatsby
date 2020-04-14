5431294090c827c9e86fb2ef6fa50c65028abcfe
Zett-8 <kame0008@gmail.com>
2020-04-14 18:12:48 +0200

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,16 @@ import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <div style={{ textAlign: 'center' }}>
-      <h1>Hello World</h1>
-      <p>“So we beat on, boats against the current, borne back ceaselessly into the past.”</p>
-    </div>
-  </Layout>
-)
+const IndexPage = () => {
+  return (
+    <Layout>
+      <SEO title="Home"/>
+      <div style={{ textAlign: 'center' }}>
+        <h1>Hello World</h1>
+        <p>“So we beat on, boats against the current, borne back ceaselessly into the past.”</p>
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
