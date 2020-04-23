044852f9a2cc2179550f377fd085b2b947e474e2
Zett-8 <kame0008@gmail.com>
2020-04-23 18:57:46 +0200

    always ignore firebase in both of build and dev phase

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,16 +10,14 @@
 // ================================================
 // ======= ignore firebase when it's being built
 exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
-  if (stage === 'build-html') {
-    actions.setWebpackConfig({
-      externals: getConfig().externals.concat(function(context, request, callback) {
-        const regex = /^@?firebase(\/(.+))?/
-        // exclude firebase products from being bundled, so they will be loaded using require() at runtime.
-        if (regex.test(request)) {
-          return callback(null, 'umd ' + request)
-        }
-        callback()
-      }),
-    })
-  }
+  actions.setWebpackConfig({
+    externals: getConfig().externals.concat(function(context, request, callback) {
+      const regex = /^@?firebase(\/(.+))?/
+      // exclude firebase products from being bundled, so they will be loaded using require() at runtime.
+      if (regex.test(request)) {
+        return callback(null, 'umd ' + request)
+      }
+      callback()
+    }),
+  })
 }
